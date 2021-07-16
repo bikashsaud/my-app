e212a58015f31d8587749fdf0f148a5c2747ff5f
bikashsaud <saudbikash514@gmail.com>
2021-07-16 16:20:23 +0545

    import methods, export other than default

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,45 @@ import { func } from 'prop-types';
 import DateTime from './DateTime';
 
 
+// import other values with default as here
+// default bahek wala lae {yo bhitra lekhne}
+
+
+// method 1 to import
+import Name, {address, SendSchool, Campus} from './DefaultImport'; 
+
+// method 2 to import this method in not widly used.
+// import * as info from "./DefaultImport";
+
+
+
 function App() {
     return(
     <>
     {/* <Heading/>
     <Body/> */}
-    <DateTime />
+    {/* <DateTime /> */}
+    <>
+    <ol>
+        
+        <li>Default Import:</li>
+        
+        {/* Method to import by using *; not widly used */}
+        {/* default Name lae default garne */}
+        {/* <li>{info.default}</li> 
+        <li>{info.address}</li>
+        <li>{info.Campus()}</li> */}
+        {/* <li> <info.SendSchool /> </li> */}
+
+
+        {/* Method1 for import one by one */}
+        <li>{Name}</li> 
+        <li>{address}</li>
+        <li>{Campus()}</li> 
+        <li>{SendSchool()}</li> 
+
+    </ol>
+    </>
     </>
     );
 }
---
+++ src/DefaultImport.jsx
@@ -0,0 +1,26 @@
+const name = "Bikash Saud";
+const address = "Baitadi Nepal."
+
+
+function SendSchool() {
+    let school1 = "Tri-Chandra Campus";
+    return school1;
+}
+function Campus() {
+    let campus = "Amrit Campus";
+    return campus;    
+}
+
+
+
+
+
+export default name;
+
+
+
+
+
+// if you want  to send more thanone values send as below save values what you define above.
+
+export {address, SendSchool, Campus};
