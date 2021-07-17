5bd7b1797b8de45db952840cbdd3de511ac6fc54
bikashsaud <saudbikash514@gmail.com>
2021-07-17 18:51:33 +0545

    digital clock and click by using useState and useInterval

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SimpleHook from './hooks/SimpleHook'
 
 const App = () =>{
+        let ftime = new Date().toLocaleTimeString();
+        const [newtime, CurrTime] = useState(ftime)
+    
+        const UpdateTime = () =>{
+            CurrTime(new Date().toLocaleTimeString())
+        }
+        setInterval(UpdateTime, 1)
     return (<>
+        <h4 style = {
+            {textAlign:"center"}
+        }>{newtime}</h4>
+
     <SimpleHook />
     </>);
 }
