d3a639f33b291717c4f46a6660d23e40992ede37
bikashsaud <saudbikash514@gmail.com>
2021-07-17 06:29:20 +0545

    props and mapping function

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,60 @@ import React from 'react';
 import SeriesCard from './props/Props';
 import constData from './props/constData';
 
-
-
-function App() {
+function SeriesData(val){
     return(
-    <>
-    {/* Props */}
-    {/* do not use researve keyword in props... */}
-    < SeriesCard imgsrc = {constData[0].imgsrc} title ={constData[0].title}  link ={constData[0].link} ></SeriesCard>
-    < SeriesCard imgsrc = {constData[1].imgsrc} title ={constData[1].title}  link ={constData[1].link} ></SeriesCard>
-    < SeriesCard imgsrc = {constData[2].imgsrc} title ={constData[2].title}  link ={constData[2].link} ></SeriesCard>
-    < SeriesCard imgsrc = {constData[3].imgsrc} title ={constData[3].title}  link ={constData[3].link} ></SeriesCard>
-
-
-    </>
+    <SeriesCard 
+    imgsrc = {val.imgsrc} 
+    title ={val.title}  
+    link ={val.link}
+    />
     );
 }
 
-export default App;
+function App(){
+    return(
+    <>
+    <h3>First Method</h3>
+    {constData.map(SeriesData)};
+     {/* second method */}
+     <h4>Second Method</h4>
+    {constData.map(function SeriesData(val) {
+            return(
+                <SeriesCard 
+                imgsrc = {val.imgsrc} 
+                title ={val.title}  
+                link ={val.link}
+                />
+                );
+    })}
+    {/* third method FAT Arrow Functino */}
+    <h4>3rd Method </h4>
+    {constData.map((val, index)=> {
+        console.log(index)
+        return(
+            <SeriesCard 
+            imgsrc = {val.imgsrc} 
+            title ={val.title}  
+            link ={val.link}
+            />
+        )
+    })}
+    <h5>difference between FAT arrow and Function</h5>
+    {dataAdd()}
+    <br />
+    {dataRemove()}
+    </>)
+}
+
+export default App; 
+
+
+// difference between function and fat arrow function 
+
+function dataAdd(val){
+    return (
+        <h1>Bikash Added</h1>
+    );
+}
+
+const dataRemove = (val) => <h1>Bkash Remove</h1> 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import reactDom from "react-dom";
 
-import App from './App'
+import App from './App';
 
 reactDom.render(<App />,
     document.getElementById('root')
