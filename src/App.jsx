27986736727a8a7c1192e06671716728e1a9f6cb
bikashsaud <saudbikash514@gmail.com>
2021-07-17 21:53:39 +0545

    events handling by using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SimpleHook from './hooks/SimpleHook'
 
 const App = () =>{
+        let ftime = new Date().toLocaleTimeString();
+        const [newtime, CurrTime] = useState(ftime)
+    
+        const UpdateTime = () =>{
+            CurrTime(new Date().toLocaleTimeString())
+        }
+        setInterval(UpdateTime, 1)
     return (<>
+        <h4 style = {
+            {textAlign:"center"}
+        }>{newtime}</h4>
+
     <SimpleHook />
     </>);
 }
 
 export default App; 
+
 
 
--- src/hooks/SimpleHook.jsx
+++ src/hooks/SimpleHook.jsx
@@ -6,12 +6,25 @@ import "./SimpleHook.css";
 
 
 const SimpleHook = ()=>{
- 
+
+    const base = "teal"  
+    const [colorBase, changeBase] = useState(base)
+    const [newText, UpdateText] = useState('Click Me 👆')
+    const bgButton = ()=> {
+        changeBase('blue');
+        UpdateText('Outch!! 😫 ') 
+        
+    }
+
+    const bg2Click = () =>{
+        changeBase('teal')
+        UpdateText('Ayyoo!!! 😠')
+    }
+
     return (
     <>
-    <div className="main_hook">
-        
-        <button onClick={IncNum}>Click Me</button>
+    <div className="main_hook" style = { { backgroundColor: colorBase}}>
+        <button onDoubleClick={bg2Click} onClick={bgButton}> {newText} </button>
     </div>
     </>    )
 }
